bdcfbcbfc2bf1eed73914574e3c64b9856ca1710
Muhammad Hashir <hashirn77@gmail.com>
2026-07-30 17:15:18 +0500

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,8 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+};
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
