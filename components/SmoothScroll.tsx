1346f41a1dc509b34f89389b0bfc7b70fb45129e
Muhammad Hashir <hashirn77@gmail.com>
2026-07-30 13:37:31 +0500

    Custom order page and smooth scroll fix

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,10 +6,12 @@ import Lenis from "lenis";
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.0,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       syncTouch: false,
-      touchMultiplier: 2,
+      touchMultiplier: 1.5,
+      wheelMultiplier: 1,
     });
     function raf(time: number) {
       lenis.raf(time);
